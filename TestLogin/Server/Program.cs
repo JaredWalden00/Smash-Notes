2fcf2894b264e51303daadbef7192420f028935d
JaredWalden00 <jaredwalden00@gmail.com>
2024-05-03 16:05:41 -0400

    Revert "Added Azure Connection String"
    This reverts commit 484998f3be29dc888f8edbd301a2dcc564d05f4c.

--- TestLogin/Server/Program.cs
+++ TestLogin/Server/Program.cs
@@ -15,10 +15,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddSwaggerGen();
-string ConnectionString = @"Server=smashnotes.database.windows.net; Authentication=Active Directory Interactive; Database=testlogin";
 builder.Services.AddDbContext<DataContext>(options =>
-    options.UseSqlServer(
-        ConnectionString));
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllersWithViews();
 builder.Services.AddSwaggerGen(c => {
 	c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
